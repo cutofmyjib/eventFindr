a4b186f49d983841ec8c5309fdfa52fe1e35f3c1
cutofmyjib <thedianalozano@gmail.com>
2016-01-31 00:10:57 -0800

    add state, create onKeyDown property

--- app/home.js
+++ app/home.js
@@ -1,15 +1,31 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
-import Eventsearch from './eventsearch.js'
+import {connect} from 'react-refetch';
+import Eventsearch from './eventsearch.js';
+import Events from './events.js';
 
 export default class Home extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  setLoc(event) {
+    if(event.keyCode == 13) {
+      this.setState({ queryString: event.target.value });
+    }
+  }
+
   render() {
     return (
-      <section className="landing">
-        <h1 className="appname">eventFindr</h1>
-        <p>Find events near you!</p>
-        <Eventsearch />
-      </section>
+      <div>
+        <section className="search-header">
+          <h1 className="appname">eventFindr</h1>
+          <p>Find events near you!</p>
+          <Eventsearch onKeyDown={this.setLoc.bind(this)} />
+        </section>
+        <Events />
+      </div>
     );
   }
 }
