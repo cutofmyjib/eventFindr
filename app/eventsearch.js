290b812c5b11b2bcce86bb9b3371bee08d07dc30
cutofmyjib <thedianalozano@gmail.com>
2016-01-30 20:44:16 -0800

    change onKeyDown to be passed through props

--- app/eventsearch.js
+++ app/eventsearch.js
@@ -1,22 +1,14 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
+import Events from './events.js'
 
 export default class Eventsearch extends Component {
-  searchEvents(event) {
-    if(event.keyCode == 13) {
-      console.log("hi");
-    }
-  }
-
   render() {
     return (
-      <div>
-        <div className="ui category search">
-          <div className="ui icon input">
-            <input className="prompt" type="text" onKeyDown={this.searchEvents} placeholder="San Francisco, CA" />
-            <i className="search icon"></i>
-          </div>
-          <div className="results"></div>
+      <div className="ui category search">
+        <div className="ui icon input">
+          <input className="prompt" type="text" onKeyDown={this.props.onKeyDown} placeholder="San Francisco, CA" />
+          <i className="search icon"></i>
         </div>
       </div>
     );
