6508f5b49634b640002e80588e12893dac020460
cutofmyjib <thedianalozano@gmail.com>
2016-01-30 20:42:22 -0800

    add router

--- app/App.js
+++ app/App.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
+import { hashHistory, Router, Route, Link } from 'react-router'
 import Home from './home.js'
 
 class App extends Component {
   render() {
     return (
-      <div className="wrapper">
-        <Home />
-      </div>
+      <Router history={hashHistory}>
+        <Route path="/" component={Home}/>
+      </Router>
     );
   }
 }
