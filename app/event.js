0ef01d0caf7c1ee8821e30af04cf8ec3ccc5dd48
cutofmyjib <thedianalozano@gmail.com>
2016-01-31 22:19:39 -0800

    add moment.js and dates

--- app/event.js
+++ app/event.js
@@ -1,9 +1,12 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
 import {Link} from 'react-router';
+var moment = require('moment');
 
 export default class Event extends Component {
   render() {
+    var start = moment(this.props.start.utc).format('MMM. D, YYYY h:mm a');
+    var end = moment(this.props.end.utc).format('MMM. D, YYYY h:mm a');
     return (
       <a className="yellow card" href={this.props.url} target="_blank">
         <div className="ui middle aligned selection list">
@@ -11,6 +14,7 @@ export default class Event extends Component {
           <img src={this.props.logo ? this.props.logo.url : 'style/defaultBG.png'} className="ui tiny image" />
           <div className="content">
             <div className="header">{this.props.name.text}</div>
+            <div className="meta">{start} - {end}</div>
           </div>
         </div>
       </div>
