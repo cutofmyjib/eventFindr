64c172f66639109e854732ea9f488b6a99e3e7f1
cutofmyjib <thedianalozano@gmail.com>
2016-01-31 20:50:59 -0800

    add status state, use in message

--- app/events.js
+++ app/events.js
@@ -12,6 +12,7 @@ export default class Events extends Component {
   }
 
   componentWillReceiveProps(nextProps) {
+    this.setState({status: 'loading'})
     reqwest({
       url: base+'venue.city='+nextProps.queryCity+'&token='+auth,
       //CORS not jsonp
@@ -19,22 +20,30 @@ export default class Events extends Component {
       type: 'json'
     })
     .then((function(response){
-      this.setState({eventsQuery: response.events});
+      this.setState({eventsQuery: response.events, status: 'success'});
     }).bind(this))
 
     .catch(function(response){
-      console.log('fail');
-      console.log(response)
+      this.setState({status: 'fail'})
     })
   }
 
   render() {
+    var message = '';
     var list = this.state.eventsQuery.map(function(data) {
       return <Event {...data} />
     })
+    if (this.state.status === 'success') {
+      message = 'Here are events in ' + this.props.queryCity;
+    } else {
+      message = this.state.status;
+    }
     return (
-      <div className="ui stackable cards">
-        {list}
+      <div className="events-body">
+        <h3 className="ui header">{message}</h3>
+        <div className="ui stackable cards">
+          {list}
+        </div>
       </div>
     );
   }
