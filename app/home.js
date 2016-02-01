089631251e8b0e44e0ae13e161e5d58d9e215fce
cutofmyjib <thedianalozano@gmail.com>
2016-02-01 04:15:58 -0800

    add radiobuttons component

--- app/home.js
+++ app/home.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
 import Eventsearch from './eventsearch.js';
+import Radiobuttons from './radiobuttons.js';
 import Events from './events.js';
 
 export default class Home extends Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {selectedWeek: 'this_week'}
   }
 
   setLoc(event) {
@@ -22,6 +23,7 @@ export default class Home extends Component {
           <h1 className="appname">eventFindr</h1>
           <p>Find events near you!</p>
           <Eventsearch onKeyDown={this.setLoc.bind(this)} />
+          <Radiobuttons />
         </section>
         <section className="events-body">
           <Events queryCity={this.state.queryCity}/>
