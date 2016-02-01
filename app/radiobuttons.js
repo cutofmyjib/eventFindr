285118551207403b32720739136f085c8699339f
cutofmyjib <thedianalozano@gmail.com>
2016-02-01 05:41:33 -0800

    import component to home, add property componentWillReceiveProps

--- app/radiobuttons.js
+++ app/radiobuttons.js
@@ -4,26 +4,19 @@ import {render} from 'react-dom';
 export default class Radiobuttons extends Component {
   constructor(props) {
     super(props)
-    this.state = {week: 'this_week'}
+    this.state = {}
   }
 
-  // componentWillReceiveProps(nextProps) {
-  //   this.setState({selectedWeek: nextProps.selectedWeek})
-  // }
-
-  thisWeek(event) {
-    this.setState({week: event.target.value})
-  }
-  nextWeek(event) {
-    this.setState({week: event.target.value})
+  componentWillReceiveProps(nextProps) {
+    this.setState({week: nextProps.week})
   }
 
   render() {
     return (
       <div className="radio-form">
-        <input type="radio" value="this_week" checked={this.state.week === 'this_week'} onChange={this.thisWeek.bind(this)} />
+        <input type="radio" value="this_week" checked={this.props.checked == 'this_week'} onChange={this.props.onChange} />
         <label>This Week</label>
-        <input type="radio" value="next_week" checked={this.state.week === 'next_week'} onChange={this.nextWeek.bind(this)} />
+        <input type="radio" value="next_week" checked={this.props.checked == 'next_week'} onChange={this.props.onChange} />
         <label>Next Week</label>
       </div>
     );
