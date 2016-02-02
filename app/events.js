87411d0bac409f1d71ba5aa0e78addad80ff17f5
cutofmyjib <thedianalozano@gmail.com>
2016-02-01 21:20:07 -0800

    add logic for next weekend

--- app/events.js
+++ app/events.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import reqwest from 'reqwest';
 import {Loading, Success, Empty, Error} from './status.js';
 import {base, auth} from './creds.js';
+import moment from 'moment';
 
 export default class Events extends Component {
   constructor(props) {
@@ -13,16 +14,27 @@ export default class Events extends Component {
     if (!(nextProps.city && nextProps.week)) {
       return
     }
+
     this.setState({status: 'loading'})
+
+    var data = {
+      'venue.city': nextProps.city,
+      'sort_by': 'date',
+      'token': auth
+    }
+    if (nextProps.week === 'next_weekend') {
+      const dateTime = 'YYYY-MM-DDTHH:mm:ss';
+      const baseDate = moment().add(1, 'week').endOf('week');
+      data['start_date.range_start'] = moment(baseDate).startOf('day').utc().format(dateTime);
+      data['start_date.range_end'] = moment(baseDate).add(1, 'day').endOf('day').utc().format(dateTime);
+    } else {
+      data['start_date.keyword'] = nextProps.week;
+    }
+
     reqwest({
       method: 'GET',
       url: base,
-      data: {
-        'venue.city': nextProps.city,
-        'sort_by': 'date',
-        'start_date.keyword': nextProps.week,
-        'token': auth
-      },
+      data: data,
       type: 'json',
       //CORS not jsonp
       crossOrigin: true
