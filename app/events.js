a0251382a8fd8035b3cdad7812f69ca89f409a48
cutofmyjib <thedianalozano@gmail.com>
2016-02-01 19:54:12 -0800

    add data property in reqwest, import status components, refactor if to switch

--- app/events.js
+++ app/events.js
@@ -1,9 +1,7 @@
 import React, {Component} from 'react';
-import {render} from 'react-dom';
 import reqwest from 'reqwest';
-import Event from './event.js';
+import {Loading, Success, Empty, Error} from './status.js';
 import {base, auth} from './creds.js';
-import {Link} from 'react-router';
 
 export default class Events extends Component {
   constructor(props) {
@@ -12,47 +10,45 @@ export default class Events extends Component {
   }
 
   componentWillReceiveProps(nextProps) {
-
-    if (nextProps.queryCity && nextProps.week) {
-      this.setState({status: 'loading'})
-      reqwest({
-        url: base+'venue.city='+nextProps.queryCity+'&sort_by=date&start_date.keyword='+nextProps.week+'&token='+auth,
-        //CORS not jsonp
-        crossOrigin: true,
-        type: 'json'
-      })
-      .then((function(response){
-        this.setState({eventsQuery: response.events, status: 'success'});
-      }).bind(this))
-
-      .catch((function(response){
-        this.setState({status: 'fail'})
-      }).bind(this))
+    if (!(nextProps.city && nextProps.week)) {
+      return
     }
+    this.setState({status: 'loading'})
+    reqwest({
+      method: 'GET',
+      url: base,
+      data: {
+        'venue.city': nextProps.city,
+        'sort_by': 'date',
+        'start_date.keyword': nextProps.week,
+        'token': auth
+      },
+      type: 'json',
+      //CORS not jsonp
+      crossOrigin: true
+    })
+
+    .then((function(response){
+      this.setState({eventsQuery: response.events, status: 'success'});
+    }).bind(this))
+
+    .catch((function(response){
+      this.setState({status: 'fail'})
+    }).bind(this))
   }
 
   render() {
-    var message = '';
-    var list = this.state.eventsQuery.map(function(data) {
-      return <Event {...data} />
-    })
-    if (this.state.status === 'success') {
-      if (list.length > 0) {
-        message = 'Popular events in ' + this.props.queryCity;
-      } else {
-        message = 'Sorry, there are no events this week.'
-      }
-    } else {
-      message = this.state.status;
+    switch (this.state.status) {
+      case 'success':
+        return (this.state.eventsQuery.length > 0)
+          ? <Success events={this.state.eventsQuery} city={this.props.city} />
+          : <Empty />
+      case 'loading':
+        return <Loading />
+      case 'fail':
+        return <Error />
+      default:
+        return <div></div>
     }
-    return (
-      <div>
-        <h1 className="events-status">{message}</h1>
-        <div className="ui stackable cards">
-          {list}
-        </div>
-      </div>
-    );
   }
-
 }
