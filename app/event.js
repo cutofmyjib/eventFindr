d96a29a5187d90953f027a1ddc059cb8f1e15b92
cutofmyjib <thedianalozano@gmail.com>
2016-02-01 22:33:32 -0800

    change styling

--- app/event.js
+++ app/event.js
@@ -6,7 +6,7 @@ export default class Event extends Component {
     var start = moment(this.props.start.utc).format('MMM. D, YYYY h:mm a');
     var end = moment(this.props.end.utc).format('MMM. D, YYYY h:mm a');
     return (
-      <a className="yellow card" href={this.props.url} target="_blank">
+      <a className="ui card" href={this.props.url} target="_blank">
         <div className="ui middle aligned selection list">
           <div className="item">
           <img src={this.props.logo ? this.props.logo.url : 'style/defaultBG.png'} className="ui tiny image" />
